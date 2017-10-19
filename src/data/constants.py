eff8c3b372e4547afbd4cb7561f723739da03240
Huy <giahuy0000@yahoo.com.vn>
2017-10-19 22:15:35 +0700

    prepare test data

--- src/data/constants.py
+++ src/data/constants.py
@@ -1,9 +1,12 @@
-DATASET_NAME = 'semeval2017-task8-dataset/'
+DATASET_NAME = 'semeval2017-task8-dataset'
+TESTSET_NAME = 'semeval2017-task8-test-data'
 
 RAW_OUTPUT_FOLDER = 'traindev/'
 RAW_INPUT_FOLDER = 'rumoureval-data/'
 DATASET_EVENTS = ['charliehebdo/', 'ebola-essien/', 'ferguson/', 'germanwings-crash/', 'ottawashooting/', 'prince-toronto/', 'putinmissing/', 'sydneysiege/']
 VERACITY_LABEL_FILE = ['rumoureval-subtaskB-dev.json', 'rumoureval-subtaskB-train.json']
 STANCE_LABEL_FILE = ['rumoureval-subtaskA-dev.json', 'rumoureval-subtaskA-train.json']
+VERACITY_LABEL_TEST_FILE = ['subtaskB.json']
+STANCE_LABEL_TEST_FILE = ['subtaskA.json']
 
 DATASET_OUT = 'make_dataset'
