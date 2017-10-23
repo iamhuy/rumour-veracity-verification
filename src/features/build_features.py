54db942729035bfcac0b7794ef16d2fb08cb8dec
Huy <giahuy0000@yahoo.com.vn>
2017-10-23 17:44:56 +0700

    exclamation mark, question mark and dot features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,5 +1,6 @@
 from has_description import has_description
 from geo_enabled import geo_enabled
+from num_occurrences import num_occurrences
 
 
 def collect_feature(tweet):
@@ -16,5 +17,18 @@ def collect_feature(tweet):
 
     # Whether the user has enabled geo-location or not.
     feature_vector += geo_enabled(tweet['user'])
+
+    # Whether the tweet contain dot dot dot or not and number of dot dot dot
+    dotdotdot_occurrences = num_occurrences(tweet['text'], r'\.\.\.')
+    feature_vector += [1 if dotdotdot_occurrences > 0 else 0, dotdotdot_occurrences]
+
+    # Whether the tweet contain exclamation mark or not and number of exclamation marks
+    exclamation_mark_occurrences = num_occurrences(tweet['text'], r'!')
+    feature_vector += [1 if exclamation_mark_occurrences > 0 else 0, exclamation_mark_occurrences]
+
+    # Whether the tweet contain question mark or not and number of question marks
+    question_mark_occurrences = num_occurrences(tweet['text'], r'?')
+    feature_vector += [1 if question_mark_occurrences > 0 else 0, question_mark_occurrences]
+
 
     return feature_vector
