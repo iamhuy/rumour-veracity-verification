9e30facb875bab04bb88601f3885339b163b0c25
potus84 <tuantai.cse@gmail.com>
2017-10-30 09:29:40 +0700

    Slang_Bad_Words & Acronyms features implemented

---
+++ src/features/hasWord.py
@@ -0,0 +1,33 @@
+import pickle
+import preprocessor as p
+def readList(filename):
+    """
+    Read the saved file list containing words
+    :param filename: the name of the file
+    :return: the list of words
+    """
+    wordList=[]
+    with open(filename, 'rb') as fp:
+        wordList = pickle.load(fp)
+        #print wordList
+    return wordList
+def preprocess_tweet(tweet):
+    cleaned_tweet = tweet.lower()  # lowercase the tweet
+    p.set_options(p.OPT.URL, p.OPT.EMOJI, p.OPT.MENTION, p.OPT.HASHTAG)  # set options for the preprocessor
+    cleaned_tweet = p.clean(cleabned_tweet.encode("ascii", "ignore"))
+    return cleaned_tweet;
+
+def check_existence_of_words(tweet, wordlist):
+    """
+    Function for the slang or curse words and acronyms features
+    :param tweet: semi process tweet (hashtags mentions removed)
+    :param wordlist:List of words
+    :return: the binary vector of word in the tweet
+    """
+    tweet=preprocess_tweet(tweet)
+    boolean=0
+    for word in wordlist:
+        if (tweet.find(word) != -1):
+            boolean=1
+            break
+    return [boolean]
