a22433d116e091b886e02a268bbc2fa2b24e4152
potus84 <tuantai.cse@gmail.com>
2017-11-09 15:37:55 +0700

    ngram POS Tagging vector, n in 1-4

--- src/features/pos_tag.py
+++ src/features/pos_tag.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python
-from sklearn.feature_extraction.text import CountVectorizer
-import os
-import json
-import preprocessor as p
-import sys
-import re
-from nltk.corpus import stopwords
-from nltk.tokenize import TweetTokenizer
 import nltk
-import string
 import itertools
+from utils import preprocess_tweet
 
 
-def preprocess_tweet(tweet):
-    cleaned_tweet = tweet.lower()  # lowercase the tweet
-    p.set_options(p.OPT.URL, p.OPT.EMOJI, p.OPT.MENTION, p.OPT.HASHTAG)  # set options for the preprocessor
-    cleaned_tweet = p.clean(cleaned_tweet.encode("ascii", "ignore"))
-
-    return cleaned_tweet;
-
-
-def get_bigram_postag_vector(tweet):
+def prepare_tag(n):
     """
-    Return the bi-gram POStagging of the tweet
-    :param tweet: A nonpreprocessed tweet
-    :return: a universal postagging, bigram vector
+    Prepare the combination of the tagset
+    :param n: the number of gram
+    :return: the tag set relating to n
     """
     tag_set = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRT', 'PRON', 'VERB', '.', 'X']
-    bigram_tag = []
-    for i in itertools.product(tag_set, tag_set):
-        bigram_tag.append(str(i))
+    ngram_tag=[]
+    if n == 1:
+        for i in tag_set:
+            ngram_tag.append("('"+i+"')")
+    elif n == 2:
+        for i in itertools.product(tag_set, tag_set):
+            ngram_tag.append(str(i))
+    elif n == 3:
+        for i in itertools.product(tag_set, tag_set, tag_set):
+            ngram_tag.append(str(i))
+    elif n == 4:
+        for i in itertools.product(tag_set, tag_set, tag_set, tag_set):
+            ngram_tag.append(str(i))
+    return ngram_tag
 
+def get_ngram_postag_vector(tweet, n):
+    """
+    Return the ngram POStagging vector of the tweet
+    :param tweet: A nonpreprocessed tweet
+    :param n: the number of gram in range [1,4]
+    :return: Vector of ngram tagging using Universal tagging
+    """
+    #prepare the tag
+    ngram_tag = prepare_tag(n)
     #preprocess tweet, remove emoticons, hashtags, metions
     tweet=preprocess_tweet(tweet)
 
@@ -38,42 +42,16 @@ def get_bigram_postag_vector(tweet):
     token = nltk.word_tokenize(tweet)
     tagged_token = nltk.pos_tag(token, tagset="universal")
 
-    #create the vector size of bigram_tag
-    pos_vector = [0] * len(bigram_tag)
+    #create the vector size of ngram_tag
+    pos_vector = [0] * len(ngram_tag)
 
     #check tag and return vector
-    for i in range(0, (len(tagged_token) - 1)):
-        pos_vector[
-            (bigram_tag.index(str("('" + tagged_token[i][1] + "'" + ", " + "'" + tagged_token[i + 1][1] + "')")))] = 1
+    for i in range(0, (len(tagged_token) - n + 1)):
+        str_list = []
+        for j in range(0, n):
+            str_list.append("'" + tagged_token[i+j][1] + "'")
+        str1=", ".join(str_list)
+        str="("+str1+")"
+        pos_vector[(ngram_tag.index(str))] = 1
 
     return pos_vector
-
-
-def get_trigram_postag_vector(tweet):
-    """
-    Return the bi-gram POStagging of the tweet
-    :param tweet: A nonpreprocessed tweet
-    :return: a universal postagging, bigram vector
-    """
-    tag_set = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRT', 'PRON', 'VERB', '.', 'X']
-    trigram_tag = []
-    for i in itertools.product(tag_set, tag_set,tag_set):
-        trigram_tag.append(str(i))
-
-    #preprocess tweet, remove emoticons, hashtags, metions
-    tweet=preprocess_tweet(tweet)
-
-    #tokenize tweet
-    token = nltk.word_tokenize(tweet)
-    tagged_token = nltk.pos_tag(token, tagset="universal")
-
-    #create the vector size of bigram_tag
-    pos_vector = [0] * len(trigram_tag)
-
-    #check tag and return vector
-    for i in range(0, (len(tagged_token) - 2)):
-        pos_vector[
-            (trigram_tag.index(str("('" + tagged_token[i][1] + "'" + ", " + "'" + tagged_token[i + 1][1] + "'" + ", " + "'" + tagged_token[i + 2][1]+"')")))] = 1
-
-    return pos_vector
-
